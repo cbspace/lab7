eea74dadbf5c4cea5f9029cbacc994cd7738efd1
cbspace <24204459+cbspace@users.noreply.github.com>
2026-08-01 14:49:51 +1000

    Fixed Prisma Errors

--- w7dockerp2/frontend/next.config.ts
+++ w7dockerp2/frontend/next.config.ts
@@ -4,4 +4,8 @@ const nextConfig: NextConfig = {
   /* config options here */
 };
 
+module.exports = {
+  allowedDevOrigins: ['10.0.0.100', '10.0.0.119'], /*change to your IP */
+}
+
 export default nextConfig;
